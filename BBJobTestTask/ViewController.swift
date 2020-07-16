65605cbfd3708c8359fa3274213253c5fff8eb62
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-07-16 22:04:37 +0300

    Initial Commit

---
+++ BBJobTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BBJobTestTask
+//
+//  Created by Maksym Sabadyshyn on 7/16/20.
+//  Copyright © 2020 Maksym Sabadyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
